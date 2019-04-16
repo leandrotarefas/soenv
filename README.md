7457a722205b161e0d7026a8b2c48bce32140639
leandrotarefas <conta.tarefas@gmail.com>
2019-04-16 13:53:09 -0300

    inclusao de path para logs

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # soenv
 Manage enviorement variable (.env) between Windows and Linux
 
-## Config File (.env) [required] 
+## Config File (.env) [required] have ***four*** required keys
 
 keys ***path_env_win*** and ***path_env_linux*** are required
+
 
 set with path for your app .env 
 
 ### .env (local file) with example path
 
 ***path_env_win***=D:/data/myapp/env/app.env  
+
 ***path_env_linux***=/data/myapp/env/app.env
+
+***path_env_other*** = other so path
+
+
+set with path for your LOG app
+
+***log_path_env_linux***=D:/data/myapp/logs/
+
+***log_path_env_win***=/data/myapp/logs/
+
+***log_path_env_other***=other so path
+
 
 ### app.env
 
@@ -32,6 +46,7 @@ const SOENV = require("soenv");
 const env = new SOENV();
 
 env.getValue('APP');
+
 env.getValue('HOSTNAME');
 
 
--- app.js
+++ app.js
@@ -7,14 +7,31 @@ function MyEnv() {
     let path_value = "";
     if (os == "linux"){
         path_value = process.env.path_env_linux;
-    }else{
+    }else if(os == "win32"){
         path_value = process.env.path_env_win;
+    }else{
+        path_value = process.env.path_env_other
     }
+
     require('dotenv').config({path: path_value})  
 }
 
 MyEnv.prototype.getValue = function(key) {
     return process.env[key]
+}
+
+MyEnv.prototype.getLogPath = function(key) {
+    const os = process.platform;
+
+    let path_value = "";
+    if (os == "linux"){
+        path_value = process.env.log_path_env_linux;
+    }else if(os == "win32"){
+        path_value = process.env.log_path_env_win;
+    }else{
+        path_value = process.env.log_path_env_other
+    }
+    
 }
 
 module.exports = MyEnv;
